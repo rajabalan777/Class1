3576b4057e36e3c98ebc440bde4704320efae56b
rajabalan777 <59643923+rajabalan777@users.noreply.github.com>
2020-01-10 18:00:45 +0000

    finalized project

---
+++ hand cricket in python.py
@@ -0,0 +1,59 @@
+import random
+overs=int(input("How many overs would you like to play?\n"))
+print("The game is set for",overs,"overs")
+balls=overs*6
+user_total=0
+count=1
+target=user_total+1
+print("you're going to bat now")
+while count<=balls:
+    user=int(input("Enter number \n"))
+    if user<=6:
+        print("You've inputed",user,"\n")
+    else:
+        print('Enter number from 1 to 6')
+        continue
+    comp = random.randint(1,6)
+    print("Computer chose",comp,"\n")
+    if (comp==user):
+        print("OUT :¬( ","\n")
+        break
+    else:
+        user_total=user_total+user
+        target=user_total+1
+        print("Your total is now",user_total)
+    i=1
+    count=count+i
+print("Your score is:",user_total)
+print('target:',target)
+com_total=0
+count=1
+remaining=target-com_total
+print("you're going to bowl now")
+while count<=balls:
+    user=int(input("Enter number \n"))
+    if user<=6:
+        print("You've inputed",user,"\n")
+    else:
+        print('enter number from 1 to 6')
+        continue
+    comp=(random.randint(1,6))
+    print("Computer chose",comp,"\n")
+    if (comp==user):
+        print("OUT :¬)")
+        break
+    else:
+        com_total=com_total+comp
+        remaining=target-com_total
+        print("computer's score is now:",com_total)
+    if remaining<=0:
+        print('computer won')
+        break
+    i=1
+    count=count+i
+if user_total>com_total:
+   print('you win')
+elif user_total==com_total:
+    print('tie')
+
+
